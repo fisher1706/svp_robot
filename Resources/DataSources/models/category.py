327eb468fab55e5d122335b4185b9cdf607c11cd
Oleh Lazeba <oleh.lazeba@cd.p2h.com>
2023-07-11 14:52:43 +0300

    fix remarks from robocop

--- Resources/DataSources/models/category.py
+++ Resources/DataSources/models/category.py
@@ -1,12 +1,12 @@
 class Category:
 
     def __init__(self, code, category_status, english_name, arabic_name, test_type,
-                 prometric_cbt_exam_weight, cbt_exam_weight, prometric_code):
+                 prometric_cbt_exam_weight, exam_weight, prometric_code):
         self.code = code
         self.category_status = category_status
         self.english_name = english_name
         self.arabic_name = arabic_name
         self.test_type = test_type
         self.prometric_cbt_exam_weight = prometric_cbt_exam_weight
-        self.cbt_exam_weight = cbt_exam_weight
+        self.exam_weight = exam_weight
         self.prometric_code = prometric_code
