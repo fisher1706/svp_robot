dddf3902a57c8db7bb21b17f69134b09a40bba3e
Oleh Lazeba <oleh.lazeba@cd.p2h.com>
2023-08-16 10:09:01 +0300

    test sertificate balance

--- POM/Locators/AdminCertificateBalanceLocators.py
+++ POM/Locators/AdminCertificateBalanceLocators.py
@@ -3,8 +3,8 @@ class AdminCertificateBalanceLocators:
     BTN_CANCEL = "//button[text() = 'Cancel']"
     SELECT_OWNER_ROLE = '//*[@id="status"]'
     # SELECT_COUNTRY = '//*[@id="country"]'
-    # SELECT_COUNTRY = 'select[id="country"]'
-    SELECT_COUNTRY = "#country"
+    SELECT_COUNTRY = "select[id='country']"
+    # SELECT_COUNTRY = "#country"
     FIELD_OWNER_NAME = '//*[@id="id"]'
     BTN_SEARCH = '//*[text()="Search"]'
     LAST_ENTRY = 'tbody tr:nth-child(1)'
